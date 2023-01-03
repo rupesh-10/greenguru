a69cae8951cf0cb5599a4410f4cef69d7d237a03
Rupesh Dhakal <53339816+rupesh-10@users.noreply.github.com>
2023-01-03 22:49:02 +0545

    Feat: Added Home Screen

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View,Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        backgroundColor: '#AFF3AA',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Image
+        source={{
+          uri: 'https://res.cloudinary.com/dqrrkueir/image/upload/v1672686426/greenguru/image-removebg-preview_ylexxr.png',
+        }}
+        style={{width: 250, height: 270}}
+      />
     </View>
   );
 }
